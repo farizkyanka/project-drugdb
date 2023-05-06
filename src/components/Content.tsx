00891ba1b54ca39e21133f1ed0a753d4a8320102
farizkyanka <110733906+farizkyanka@users.noreply.github.com>
2023-05-06 20:09:05 +0700

    layout improvement

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,34 +1,36 @@
-import DataModel from "../models/DataModel"
+import DataModel from '../models/DataModel';
 
-const Content: React.FC<{items: DataModel}> = (props) => {
-    return <>
-        <section className="sm:container m-10 p-4 mx-auto bg-stone-600 text-slate-200 rounded-lg">
-                    <div className="container mx-auto">
-                <div className="flex flex-row flex-wrap py-4">
-                    <aside className="w-full sm:w-1/3 md:w-1/4 px-2">
-                        <div className="sticky top-0 p-4 w-full">
-                        <img src={props.items.img} className="max-h-80"/>
-                <div className="bg-white w-100% text-black ">{props.items.name}</div>
-                <div className="bg-white w-100% text-black ">{props.items.composition}</div>
-                <div className="bg-white w-100% text-black ">{props.items.form}</div>
-                <div className="bg-white w-100% text-black ">{props.items.category}</div>
-                <div className="bg-white w-100% text-black ">{props.items.manufacturer}</div>
-                        </div>
-                    </aside>
-                    <main role="main" className="w-full sm:w-2/3 md:w-3/4 pt-1 px-2">
-                    <div className="bg-white w-100% text-black rounded-md">{props.items.indications}</div>
-                <div className="bg-white w-100% text-black rounded-md">{props.items.dose}</div>
-                <div className="bg-white w-100% text-black rounded-md">{props.items.pregnancyCategory}</div>
-                <div className="bg-white w-100% text-black rounded-md">{props.items.lactationSafety}</div>
-                    </main>
-                </div>
-            </div>
-            <div className="flex justify-center gap-4" id="safety-col">
-                <div className="bg-white w-100% text-black rounded-md">{props.items.contraIndications}</div>
-                <div className="bg-white w-100% text-black rounded-md">{props.items.adverseEffects}</div>
-            </div>
-        </section>
+const Content: React.FC<{ items: DataModel }> = (props) => {
+  return (
+    <>
+      <section className="container max-w-screen-lg m-10 mx-auto bg-stone-600 text-slate-200 ">
+        <div className="grid md:grid-cols-12 text-center">
+          <div className="md:col-span-4 flex flex-wrap justify-center bg-blue-700 text-center p-10">
+            <img src={props.items.img} className="w-full" />
+            <h2 className="w-full">{props.items.name}</h2>
+            <h2 className="w-full">{props.items.composition}</h2>
+            <h2 className="w-full">{props.items.form}</h2>
+          </div>
+          <div className="md:col-span-6 bg-red-500 md:text-left">
+            <p>{props.items.pregnancyCategory}</p>
+          </div>
+          <div className="md:col-span-2 bg-green-500 md:text-left">column</div>
+        </div>
+      </section>
     </>
+  );
 };
 
 export default Content;
+// {props.items.img}
+// {props.items.name}
+// {props.items.composition}
+// {props.items.form}
+// {props.items.category}
+// {props.items.manufacturer}
+// {props.items.indications}
+// {props.items.dose}
+// {props.items.pregnancyCategory}
+// {props.items.lactationSafety}
+// {props.items.contraIndications}
+// {props.items.adverseEffects}
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,52 @@
 export default function Navbar() {
-    return <>
-        <header className="flex flex-wrap sm:justify-start sm:flex-nowrap z-50 w-full bg-white text-sm py-4 dark:bg-gray-800">
-        <nav className="max-w-[85rem] w-full mx-auto px-4 sm:flex sm:items-center sm:justify-between" aria-label="Global">
-            <input type="text" className="rounded-full items-center px-3 py-2 mx-auto w-1/2" placeholder="Search for drug name, composition, brand, etc..."/>
+  return (
+    <>
+      <header className="w-full bg-white text-sm py-4 dark:bg-gray-800">
+        <nav
+          className="container flex justify-center max-w-screen-lg mx-auto md:px-4"
+          aria-label="Global"
+        >
+          <form className="w-full md:w-1/2">
+            <label
+              htmlFor="default-search"
+              className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
+            >
+              Search
+            </label>
+            <div className="relative">
+              <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                <svg
+                  aria-hidden="true"
+                  className="w-5 h-5 text-gray-500 dark:text-gray-400"
+                  fill="none"
+                  stroke="currentColor"
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                  ></path>
+                </svg>
+              </div>
+              <input
+                type="search"
+                id="default-search"
+                className="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="Search Mockups, Logos..."
+                required
+              ></input>
+              <button
+                type="submit"
+                className="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              >
+                Search
+              </button>
+            </div>
+          </form>
         </nav>
-        </header>
+      </header>
     </>
-};
+  );
+}
